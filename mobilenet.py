e34bd2af1ea56a14ccf88ddf2b1021fe8e98746a
Marco Kemmerling <contact@kemmerling.me>
2024-10-13 20:20:51 +0200

    image preprocessing

--- mobilenet.py
+++ mobilenet.py
@@ -12,11 +12,6 @@ from tempfile import TemporaryDirectory
 plt.ion()   # interactive mode
 
 
-###
-
-
-###
-
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -47,29 +42,8 @@ print(dataset_sizes)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 ###
 
-def imshow(inp, title=None, name=None):
-    """Display image for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    plt.savefig('test.jpg' if not name else name)
 
 
-# Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
-print("classes: ", classes)
-
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-
-imshow(out, title=[class_names[x] for x in classes])
-###
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
@@ -140,52 +114,12 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         # load best model weights
         model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
     return model
-###
-
-def visualize_model(model, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            for j in range(inputs.size()[0]):
-                images_so_far += 1
-                ax = plt.subplot(num_images//2, 2, images_so_far)
-                ax.axis('off')
-                ax.set_title(f'predicted: {class_names[preds[j]]}')
-                imshow(inputs.cpu().data[j])
-
-                if images_so_far == num_images:
-                    model.train(mode=was_training)
-                    return
-        model.train(mode=was_training)
 
 ###
 
 model = models.mobilenet_v2(pretrained=True)
-print(model.classifier)
-
 model.classifier[1] = nn.Sequential(nn.Linear(in_features=model.classifier[1].in_features, out_features=2), nn.Softmax())
-print(model.classifier)
 
-print(model)
-#
-#
-# model_ft = models.resnet18(weights='IMAGENET1K_V1')
-# num_ftrs = model_ft.fc.in_features
-# # Here the size of each output sample is set to 2.
-# # Alternatively, it can be generalized to ``nn.Linear(num_ftrs, len(class_names))``.
-# model_ft.fc = nn.Linear(num_ftrs, 2)
-
-# model_ft = model_ft.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
@@ -207,6 +141,32 @@ dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, s
 
 model.eval()
 
+
+### save onnx
+
+torch_input = torch.randn(1, 3, 224, 224)
+onnx_program = torch.onnx.dynamo_export(model, torch_input)
+onnx_program.save("guitarid_model_v0.0.1.onnx")
+
+
+
+#
+print(class_names)
+
+
+def imshow(inp, title=None, name=None):
+    """Display image for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title, wrap=True)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.savefig('test.jpg' if not name else name)
+
 for i in range(15):
     # Get a batch of training data
     inputs, classes = next(iter(dataloaders['val']))
@@ -216,19 +176,14 @@ for i in range(15):
     print("outputs ", outputs)
     _, preds = torch.max(outputs, 1)
     print("preds ", preds)
-
+    titles = [class_names[x] + '\n ' + str(o.detach().numpy()) for x, o in zip(preds, outputs)]
     # Make a grid from batch
     out = torchvision.utils.make_grid(inputs)
 
-    imshow(out, title=[class_names[x] for x in preds], name=str(i) + '.jpg')
+    imshow(out, title=titles, name=str(i) + '.jpg')
 print("input ", inputs[0], inputs[0].min(), inputs[0].max())
+#
 
-### save onnx
-
-torch_input = torch.randn(1, 3, 224, 224)
-onnx_program = torch.onnx.export(model, torch_input, "guitarid_model_v0.0.1.onnx", opset_version=7)
-# onnx_program.save("guitarid_model_v0.0.1.onnx")
-print(class_names)
 
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1, shuffle=False, num_workers=0)
               for x in ['train', 'val']}
